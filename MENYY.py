4679c1da3299720b90b96f46d41ad4d6b7682708
Timo Noko <timonoko@users.noreply.github.com>
2023-01-31 14:49:47 +0200

    ee

--- MENYY.py
+++ MENYY.py
@@ -1,14 +1,15 @@
 
 menyy=[
-    ['PERUNAT ', 5, 15],
+    ['PERUNAT ', 6, 15],
     ['RIISI   ', 5, 20],
     ['PUURO   ', 4, 5],
     ['COFEFE  ', 3, 0],
     ['PANNARI ', 2, 5],
-    ['FISHSTIG', 1, 5],   
+    ['FISHSTIC', 1, 7],   
+    ['SPAGETTI', 1, 7],   
     ['PIENET UUNIPERUNAT', 30, 0],
     ['SUURET UUNIPERUNAT', 40, 0],
-    ['LEIPA 200C', 35, 0],   
+    ['LEIPA 200C', 30, 0],   
     ['MUNA PAISTO', 1, 2],
     ['MUNA KEITTO', 3, 2],
     ['TESTI', 1, 2]   
