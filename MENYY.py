35b58e93fbbc5a85dee69921cffa4147c14934d2
Timo Noko <timonoko@users.noreply.github.com>
2023-04-20 10:43:33 +0300

    Perunoita on eri kokoja

--- MENYY.py
+++ MENYY.py
@@ -1,12 +1,13 @@
 
 menyy=[
-    ['PERUNAT ', 6, 15],
+    ['PERUNAT ', 6, 20],
     ['RIISI   ', 5, 20],
     ['PUURO   ', 4, 5],
     ['COFEFE  ', 3, 0],
     ['PANNARI ', 2, 5],
     ['FISHSTIC', 1, 7],   
     ['SPAGETTI', 5, 12],   
+    ['ISOT PERUNAT', 5, 30],
     ['PIENET UUNIPERUNAT', 30, 0],
     ['SUURET UUNIPERUNAT', 40, 0],
     ['HIIVALEIPA 200C', 30, 0],   
