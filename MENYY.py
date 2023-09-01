8ba468f15ee44a7ea9723652580a705675d1e185
Timo Noko <timonoko@users.noreply.github.com>
2023-09-01 11:37:24 +0300

    Lidlin jättiperunat vaatii 40 minuuttia

--- MENYY.py
+++ MENYY.py
@@ -8,10 +8,11 @@ menyy=[
     ['FISHSTIC', 1, 7],   
     ['SPAGETTI', 5, 12],   
     ['ISOT PERUNAT', 5, 30],
+    ['JATTIPERUNAT', 5, 40],
     ['PIENET UUNIPERUNAT', 30, 0],
     ['SUURET UUNIPERUNAT', 40, 0],
     ['HIIVALEIPA 200C', 30, 0],   
     ['PATALEIPA 200C', 40, 0],   
     ['MUNA PAISTO', 1, 2],
-    ['MUNA KEITTO', 3, 2]
+    ['MUNA KEITTO', 4, 10]
     ]
