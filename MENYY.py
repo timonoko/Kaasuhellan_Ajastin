46700e805325291bb42ca2e30e974b310bcd9c46
Timo Noko <timonoko@users.noreply.github.com>
2023-12-30 09:58:11 +0200

    Joulukinkku

--- MENYY.py
+++ MENYY.py
@@ -2,11 +2,12 @@
 menyy=[
     ['PERUNAT ', 6, 20],
     ['RIISI   ', 5, 20],
-    ['PUURO   ', 4, 5],
+    ['PUURO   ', 3, 3],
     ['COFEFE  ', 3, 0],
     ['PANNARI ', 2, 5],
     ['FISHSTIC', 1, 7],   
     ['SPAGETTI', 5, 12],   
+    ['KINKKU', 150, 0],   
     ['ISOT PERUNAT', 5, 30],
     ['JATTIPERUNAT', 5, 40],
     ['PIENET UUNIPERUNAT', 30, 0],
