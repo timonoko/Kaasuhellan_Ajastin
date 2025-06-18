58877aa61314d5cce166dd6a52bc819bf24a5006
Timo Noko <timonoko@users.noreply.github.com>
2025-06-18 07:15:35 +0300

    säätöjä

--- MENYY.py
+++ MENYY.py
@@ -1,19 +1,16 @@
 
 menyy=[
-    ['PERUNAT ', 6, 20],
+    ['PERUNAT ', 6, 30],
     ['RIISI   ', 5, 20],
     ['PUURO   ', 3, 3],
     ['COFEFE  ', 3, 0],
+    ['MUNA', 5, 15],
     ['PANNARI ', 2, 5],
     ['FISHSTIC', 1, 7],   
     ['SPAGETTI', 5, 12],   
     ['KINKKU', 150, 0],   
-    ['ISOT PERUNAT', 5, 30],
     ['JATTIPERUNAT', 5, 40],
-    ['PIENET UUNIPERUNAT', 30, 0],
-    ['SUURET UUNIPERUNAT', 40, 0],
+    ['UUNIPERUNAT', 40, 0],
     ['HIIVALEIPA 200C', 30, 0],   
-    ['PATALEIPA 200C', 40, 0],   
-    ['MUNA PAISTO', 1, 2],
-    ['MUNA KEITTO', 5, 15]
+    ['PATALEIPA 200C', 40, 0]
     ]
